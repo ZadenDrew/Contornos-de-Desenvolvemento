ff62226d7c759ef611cd3fa6492901151c3655f3
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2017-10-06 09:44:43 +0200

    Paso dos

--- src/holamundo/Holamundo.java
+++ src/holamundo/Holamundo.java
@@ -16,6 +16,7 @@ public class Holamundo {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Segundo commit");
     }
     
 }
